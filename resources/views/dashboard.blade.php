a76a5aab9b69ef50884b1cb8401c4e0557145353
abrar <abrarhameed911@gmail.com>
2025-01-26 15:27:34 +0500

    initial commit in branch-1

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container mx-auto p-4">
+        <!-- Include the Livewire component here -->
+        @livewire('template-selection')
+    </div>
+@endsection
